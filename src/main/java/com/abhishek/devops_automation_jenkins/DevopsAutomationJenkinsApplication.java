070b7eeeae84f88b89b8c64f0a306e16c8294242
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-11-02 15:27:16 +0530

    Initial commit

---
+++ src/main/java/com/abhishek/devops_automation_jenkins/DevopsAutomationJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.devops_automation_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAutomationJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAutomationJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhishek/devops_automation_jenkins/DevopsAutomationJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.devops_automation_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
